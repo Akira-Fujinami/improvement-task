6320d6b9cff8b99235d16f64c32f2b64d2e3ea2f
Akira-Fujinami <akira.fujinami@icloud.com>
2023-03-26 16:17:09 +0900

    レイアウトの調整

--- resources/views/authenticated/bulletinboard/posts.blade.php
+++ resources/views/authenticated/bulletinboard/posts.blade.php
@@ -22,9 +22,9 @@
         <div class="post_like_status">
           <div class="mr-5 button-like">
             @if(Auth::user()->is_Like($post->id))
-            <p class="m-0"><i class="fas fa-heart un_like_btn" post_id="{{ $post->id }}"></i><span class="like_counts{{$post->id}} like-count">{{$like->likeCounts($post->id)}}</span></p>
+            <p class="m-0 button-like-count"><i class="fas fa-heart un_like_btn" post_id="{{ $post->id }}"></i><span class="like_counts{{$post->id}} like-count">{{$like->likeCounts($post->id)}}</span></p>
             @else
-            <p class="m-0"><i class="fas fa-heart like_btn" post_id="{{ $post->id }}"></i><span class="like_counts{{$post->id}} like-count">{{$like->likeCounts($post->id)}}</span></p>
+            <p class="m-0 button-like-count"><i class="fas fa-heart like_btn" post_id="{{ $post->id }}"></i><span class="like_counts{{$post->id}} like-count">{{$like->likeCounts($post->id)}}</span></p>
             @endif
           </div>
         </div>
